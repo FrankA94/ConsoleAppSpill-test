079d1dd22472de9582b35903ef12075232284dd6
FrankA94 <96749300+FrankA94@users.noreply.github.com>
2022-05-04 09:44:37 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿
+using System;
+
+namespace ConsoleAppGuess_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                string secretWord = "covid";
+                string guess = "";
+                int guessCount = 0;
+                int guessLimit = 3;
+                bool outOfGuesses = false;
+
+
+                while (guess != secretWord && !outOfGuesses)
+                {
+                    if (guessCount < guessLimit)
+                    {
+                        Console.Write("Gjett her!: ");
+                        guess = Console.ReadLine();
+                        guessCount++;
+                    }
+                    else
+                    {
+                        outOfGuesses = true;
+                    }
+
+             
+                if (outOfGuesses)
+                {
+                    Console.Write("Du tapte!");
+                }
+                else
+                {
+                    Console.Write("Du gjettet riktig!");
+                }
+
+
+            }
+           
+            Console.ReadLine();
+        }
+    }
+}
